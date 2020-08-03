b4022b3feb423c447e986a5208912f5228988e5f
ssaeedhoseini <razeasemoon@gmail.com>
2020-08-03 19:51:35 +0430

    customize admin panel.

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(UserProfile)
-admin.site.register(Category)
-admin.site.register(Article)
+
+class UserProfielAdmin(admin.ModelAdmin):
+    list_display = ['user', 'avatar', ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'cover', ]
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'cover', 'created_at', ]
+    search_fields = ['title', 'content', ]
+
+
+admin.site.register(UserProfile, UserProfielAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
