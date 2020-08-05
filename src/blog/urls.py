c2352447367f545fcc831ef7c0bbc9bea140c915
ssaeedhoseini <razeasemoon@gmail.com>
2020-08-05 23:48:55 +0430

    update name of url.

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexPage.as_view(), name='index'),
-    url(r'^contact$', views.ContactPage.as_view(), name='contact_page'),
+    url(r'^contact$', views.ContactPage.as_view(), name='contact'),
     url(r'^article/all/$', views.AllArticleAPIView.as_view(), name='all_article'),
 ]
 
